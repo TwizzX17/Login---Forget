8f71ea8a5df2266d43caea1b62349b8743135c77
JacobWistroem <34305642+JacobWistroem@users.noreply.github.com>
2019-09-19 16:35:46 +0200

    New forgot stuff

--- Serverside test/SecureMe React/SecureMe React/ClientApp/src/components/forgot-pass.jsx
+++ Serverside test/SecureMe React/SecureMe React/ClientApp/src/components/forgot-pass.jsx
@@ -3,6 +3,54 @@ import logo from '../assets/Logo.svg';
 import { Link } from 'react-router-dom';
 
 class ForgotPass extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            CreateEmail: "",
+
+        };
+    }
+
+
+    SubmitInput = () => {
+        const Email = document.getElementById("Email").value;
+        let valid = 0;
+
+        if (Email.length === null) {
+            const errorphrase = "Email field cannot be empty";
+            valid = 0;
+        } else {
+            const confirmation = "";
+        }
+
+        if (valid === 0) {
+
+            fetch('/AddUser/AddUser', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ Id: 0, Email: Email, MasterPass: "" }),
+            }).then(this._checkStatus)
+                .then(response => response.json())
+                .then((response) => {
+                    console.log(response)
+                });
+
+        }
+    }
+
+    _checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response
+        } else {
+            let error = new Error(response.statusText)
+            error.reponse = response;
+            throw error
+        }
+    }
+
+
     render() {
         return (
             
@@ -18,8 +66,8 @@ class ForgotPass extends React.Component {
                             <div class="formtitles formtitles1"></div>
                             <div id="formsandtitles">
                                 <div class="formtitles">E-mail</div>
-                                <input class="loginforms" type="text"/>
-                                <input class="loginsubmit Createsubmit" type="submit" value="Login" />
+                                <input class="loginforms" type="text" />
+                            <input onClick={this.SubmitInput} className="loginsubmit Createsubmit" type="submit" value="Login" />
                             </div>
 
                         </div>
